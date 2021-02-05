e86de67b5d163d13ad6ea41c4b4583b1c8311333
Jaehyun Lewis <72472484+hm9811@users.noreply.github.com>
2021-02-05 01:51:04 -0800

    Issue#5 add product (#12)
    * Adding product is implemented
    
    * Styled Product added successfully message
    
    * Refactor DB code to async

--- eCommerceSite/Controllers/ProductController.cs
+++ eCommerceSite/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using eCommerceSite.Data;
 using eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,16 +21,40 @@ namespace eCommerceSite.Controllers
         /// <summary>
         /// Displays a view that lists all products
         /// </summary>
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             // Get all products from database
             //List<Product> products = _context.Products.ToList();
             List<Product>  products = 
-                (from p in _context.Products
-                 select p).ToList();
+                await (from p in _context.Products
+                 select p).ToListAsync();
 
             // Send list of products to view to be displayed
             return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add to DB
+                _context.Products.Add(p);
+                await _context.SaveChangesAsync();
+
+                TempData["Message"] = $"{p.Title} was added successfully";
+
+                // redirect back to catalog page
+                return RedirectToAction("Index");
+            }
+
+            return View();
         }
     }
 }
