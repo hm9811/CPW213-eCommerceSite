f9f70e21c0b07f2365a01a158834c57ab664de3b
hm9811 <hm981198@gmail.com>
2021-03-16 06:23:52 -0700

    Added session management for log in page

--- eCommerceSite/Controllers/UserController.cs
+++ eCommerceSite/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using eCommerceSite.Data;
 using eCommerceSite.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -48,6 +49,12 @@ namespace eCommerceSite.Controllers
 
         public IActionResult Login()
         {
+            //Check if user already logged in
+            if (HttpContext.Session.GetInt32("UserId").HasValue)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
             return View();
         }
 
@@ -59,20 +66,20 @@ namespace eCommerceSite.Controllers
                 return View(model);
             }
 
-            //UserAccount account =
-            //    await(from u in _context.UserAccounts
-            //          where (u.Username == model.UsernameOrEmail
-            //              || u.Email == model.UsernameOrEmail)
-            //              && u.Password == model.Password
-            //          select u).SingleOrDefaultAsync();
             UserAccount account =
-                await _context.UserAccounts
-                    .Where(userAcc => (userAcc.Username == model.UsernameOrEmail ||
-                                userAcc.Email == model.UsernameOrEmail) &&
-                                userAcc.Password == model.Password)
-                    .SingleOrDefaultAsync();
+                await (from u in _context.UserAccounts
+                       where (u.Username == model.UsernameOrEmail
+                           || u.Email == model.UsernameOrEmail)
+                           && u.Password == model.Password
+                       select u).SingleOrDefaultAsync();
+            //UserAccount account =
+            //    await _context.UserAccounts
+            //        .Where(userAcc => (userAcc.Username == model.UsernameOrEmail ||
+            //                    userAcc.Email == model.UsernameOrEmail) &&
+            //                    userAcc.Password == model.Password)
+            //        .SingleOrDefaultAsync();
 
-            if(account == null)
+            if (account == null)
             {
                 // Custom error message
                 ModelState.AddModelError(string.Empty, "Credentials were not found");
@@ -81,6 +88,8 @@ namespace eCommerceSite.Controllers
             }
 
             // Log user into website
+            HttpContext.Session.SetInt32("UserId", account.UserId);
+
             return RedirectToAction("Index", "Home");
         }
     }
