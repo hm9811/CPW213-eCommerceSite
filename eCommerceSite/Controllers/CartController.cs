c1003f187594629de105d5fe3541acdc9878e654
hm9811 <hm981198@gmail.com>
2021-03-17 19:53:18 -0700

    Able to store multiple items in shopping cart

--- eCommerceSite/Controllers/CartController.cs
+++ eCommerceSite/Controllers/CartController.cs
@@ -30,8 +30,23 @@ namespace eCommerceSite.Controllers
         {
             // Get product from the database
             Product p = await ProductDb.GetProductIdAsync(_context, id);
-            // Add product to cart cookie
-            string data = JsonConvert.SerializeObject(p);
+
+            const string CartCookie = "CartCookie";
+
+            // Get existing cart items
+            string existingItems = 
+                _httpContext.HttpContext.Request.Cookies[CartCookie];
+
+            List<Product> cartProducts = new List<Product>();
+            if(existingItems != null)
+            {
+                cartProducts = JsonConvert.DeserializeObject<List<Product>>(existingItems);
+            }
+
+            // Add current product to existing cart
+            cartProducts.Add(p);
+            // Add product list to cart cookie
+            string data = JsonConvert.SerializeObject(cartProducts);
             CookieOptions options = new CookieOptions()
             {
                 Expires = DateTime.Now.AddYears(1),
@@ -39,7 +54,7 @@ namespace eCommerceSite.Controllers
                 IsEssential = true
             };
 
-            _httpContext.HttpContext.Response.Cookies.Append("CartCookie", data, options);
+            _httpContext.HttpContext.Response.Cookies.Append(CartCookie, data, options);
 
             // Redirect back to previous page
             return RedirectToAction("Index", "Product");
