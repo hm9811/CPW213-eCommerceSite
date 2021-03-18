6cbd7b8a26a7b39e1ab700563e528332112881e0
hm9811 <hm981198@gmail.com>
2021-03-17 20:32:34 -0700

    Added success message when item is added to cart

--- eCommerceSite/Controllers/CartController.cs
+++ eCommerceSite/Controllers/CartController.cs
@@ -33,6 +33,8 @@ namespace eCommerceSite.Controllers
 
             CookieHelper.AddProductToCart(_httpContext, p);
 
+            TempData["Message"] = p.Title + " added successfully";
+
             // Redirect back to previous page
             return RedirectToAction("Index", "Product");
         }
