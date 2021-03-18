20a496908121d9338d4b5caa43aac48998bca9f5
Jaehyun Lewis <72472484+hm9811@users.noreply.github.com>
2021-03-17 20:14:12 -0700

    Issue#28 shopping cart (#39)
    * Add shopping cart link and icon to navigation and add to cart button for each product
    
    * A single product can now be added to cart
    
    * Able to store multiple items in shopping cart
    
    * Created basic summary page. Does not work with empty cookie
    
    * Refactored cart controller
    
    * Added a message for an empty cart on the summary page

---
+++ eCommerceSite/Controllers/CartController.cs
@@ -0,0 +1,47 @@
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ProductContext _context;
+        private readonly IHttpContextAccessor _httpContext;
+
+        public CartController(ProductContext context, IHttpContextAccessor httpContext)
+        {
+            _context = context;
+            _httpContext = httpContext;
+        }
+
+        /// <summary>
+        /// Adds a product to the shopping cart
+        /// </summary>
+        /// <param name="id">The id of the product to add</param>
+        public async Task<IActionResult> Add(int id) // Id of the product to add
+        {
+            // Get product from the database
+            Product p = await ProductDb.GetProductIdAsync(_context, id);
+
+            CookieHelper.AddProductToCart(_httpContext, p);
+
+            // Redirect back to previous page
+            return RedirectToAction("Index", "Product");
+        }
+
+        public IActionResult Summary()
+        {
+            List<Product> cartProducts = CookieHelper.GetCartProducts(_httpContext);
+            return base.View(cartProducts);
+        }
+
+    }
+}
